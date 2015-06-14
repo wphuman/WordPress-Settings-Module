f997f6837359ac9e702cad55364269d313278c10
Tang Rufus <tangrufus@gmail.com>
2015-06-14 11:00:14 +0800

    Add Plugin_Name_Option::update_option & Plugin_Name_Option::delete_option

--- includes/class-plugin-name-option.php
+++ includes/class-plugin-name-option.php
@@ -34,11 +34,66 @@ class Plugin_Name_Option {
 			return $default;
 		}
 
-		// @TODO: change plugin_name_settings
 		$plugin_options = get_option( 'plugin_name_settings', array() );
 
 		$value = isset( $plugin_options[ $key ] ) ? $plugin_options[ $key ] : $default;
 
 		return $value;
+	}
+
+	/**
+	 * Update an option
+	 *
+	 * Updates the specified option.
+	 * This is for developers to update options outside the settings page.
+	 *
+	 * WARNING: Hooks and filters will be triggered!!
+	 * @TODO: Trigger hooks & filters, pull requests welcomed
+	 *
+	 * @since 1.0.0
+	 * @return true if the option was saved or false if not
+	 */
+	static public function update_option( $key, $value ) {
+
+		if ( empty( $key ) ) {
+			return false;
+		}
+
+		// Load the options
+		$plugin_options = get_option( 'plugin_name_settings', array() );
+
+		// Update the specified value in the array
+		$plugin_options[ $key ] = $value;
+
+		// Save the options back to the DB
+		return update_option( 'plugin_name_settings', $plugin_options );
+	}
+
+	/**
+	 * Delete an option
+	 *
+	 * Deletes the specified option.
+	 * This is for developers to delete options outside the settings page.
+	 *
+	 * WARNING: Hooks and filters will be triggered!!
+	 * @TODO: Trigger hooks & filters, pull requests welcomed
+	 *
+	 * @since 1.0.0
+	 * @return true if the option was deleted or false if not
+	 */
+	static public function delete_option( $key ) {
+
+		if ( empty( $key ) ) {
+			return false;
+		}
+
+		// Load the options
+		$plugin_options = get_option( 'plugin_name_settings', array() );
+
+		// Delete the specified key
+		unset($plugin_options[ $key ]);
+
+		// Save the options back to the DB
+		return update_option( 'plugin_name_settings', $plugin_options );
 	}
 }
